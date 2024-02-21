aafaf735a7a2e9d2881db0c002dbd8c65cef3136
Vanessa Ogno <Vonessa0716@gmail.com>
2024-02-21 12:46:11 -0500

    i love emojis

---
+++ script.js
@@ -0,0 +1,14 @@
+// ID select
+var r = document.getElementById('rock'),
+
+    p = document.getElementById('paper'),
+
+    s = document.getElementById('scissors'),
+    u = document.getElementById('u'),
+
+    b = document.getElementById('bot');
+
+    
+    var rps = ["rock", "paper", "scissors"]
+    var emoji = { rock: '✊️', paper: '🖐️', scissors: '✌️' }
+    
